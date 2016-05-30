f0e092e22dea7f03dac09a595a6cfcafecc2cecc
yangjw <yangwen8701@126.com>
2016-05-30 10:06:19 +0800

    1、初始导入

---
+++ app/src/main/java/com/androidxx/yangjw/toplevel1602/MainActivity.java
@@ -0,0 +1,13 @@
+package com.androidxx.yangjw.toplevel1602;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
