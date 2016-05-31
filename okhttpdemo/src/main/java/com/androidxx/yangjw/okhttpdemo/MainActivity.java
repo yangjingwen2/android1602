dce907653c4be68c78c54fc1296d3b25db5da145
yangjw <yangwen8701@126.com>
2016-05-31 15:08:23 +0800

    OkHttp的简单使用的Demo

--- okhttpdemo/src/main/java/com/androidxx/yangjw/okhttpdemo/MainActivity.java
+++ okhttpdemo/src/main/java/com/androidxx/yangjw/okhttpdemo/MainActivity.java
@@ -6,11 +6,25 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     private static final String TAG = "androidxx";
     private Button mGetBtn;
     private Button mAysncGetBtn;
+    private Button mPostBtn;
+    private Button mJsonBtn;
+    OkHttpClient okHttp = new OkHttpClient();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,8 +32,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         mGetBtn = (Button) findViewById(R.id.okhttp_get_btn);
         mAysncGetBtn = (Button) findViewById(R.id.okhttp_get_async_btn);
+        mPostBtn = (Button) findViewById(R.id.okhttp_post_sync_btn);
+        mJsonBtn = (Button) findViewById(R.id.okhttp_json_sync_btn);
         mGetBtn.setOnClickListener(this);
         mAysncGetBtn.setOnClickListener(this);
+        mPostBtn.setOnClickListener(this);
+        mJsonBtn.setOnClickListener(this);
     }
 
     @Override
@@ -36,8 +54,65 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     }
                 });
                 break;
+            case R.id.okhttp_post_sync_btn:
+                postSync();
+                break;
+            case R.id.okhttp_json_sync_btn:
+                postJson();
+                break;
         }
 //
+
+    }
+
+    private void postJson() {
+        MediaType mediaType = MediaType.parse("application/json; charset=utf-8");
+        String json = "username/wangwu";
+        RequestBody requestBody = RequestBody.create(mediaType,json);
+        final Request request = new Request.Builder()
+                .url("http://192.168.42.1:8080/WebServer/login.do")
+                .post(requestBody)
+                .build();
+        okHttp.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                Log.d(TAG, "onResponse: " + response);
+            }
+        });
+    }
+
+    /**
+     * Post同步请求
+     */
+    private void postSync() {
+
+        /**
+         * 用来封装Post数据
+         * 表单数据
+         */
+        FormBody formBody = new FormBody.Builder()
+                .add("user","zhangsan")
+                .build();
+        Request request = new Request.Builder()
+                .post(formBody)
+                .url("http://192.168.42.1:8080/WebServer/login.do")
+                .build();
+        okHttp.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                Log.d(TAG, "onResponse: " + response.body().string());
+            }
+        });
 
     }
 }
