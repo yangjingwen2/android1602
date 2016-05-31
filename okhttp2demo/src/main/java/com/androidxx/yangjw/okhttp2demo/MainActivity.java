6e27fa57890bcad4d323e43251ecbc8684aac293
yangjw <yangwen8701@126.com>
2016-05-31 17:32:31 +0800

    OkHttp的封装框架的demo

--- okhttp2demo/src/main/java/com/androidxx/yangjw/okhttp2demo/MainActivity.java
+++ okhttp2demo/src/main/java/com/androidxx/yangjw/okhttp2demo/MainActivity.java
@@ -8,6 +8,8 @@ import android.view.View;
 import com.androidxx.yangjw.httplibrary.IOkTaskCallback;
 import com.androidxx.yangjw.httplibrary.OkHttpTask;
 
+import java.util.HashMap;
+
 public class MainActivity extends AppCompatActivity implements IOkTaskCallback{
 
     private static final String TAG = "androidxx";
@@ -21,7 +23,13 @@ public class MainActivity extends AppCompatActivity implements IOkTaskCallback{
     public void click(View view) {
         switch (view.getId()) {
             case R.id.ok_get_btn:
-                OkHttpTask.newInstance().enqueue(this).start("eeeeeeeee");
+                OkHttpTask.newInstance(this).enqueue(this).start("http://www.baidu.com");
+                OkHttpTask.newInstance(this).enqueue(this).start("http://www.baidu.com");
+                break;
+            case R.id.ok_post_btn:
+                HashMap<String,String> map = new HashMap<>();
+                map.put("user","android1602");
+                OkHttpTask.newInstance(this).post(map, OkHttpTask.OkHttpTaskBuidler.OkType.json).enqueue(this).start("http://192.168.42.1:8080/WebServer/login.do");
                 break;
         }
     }
