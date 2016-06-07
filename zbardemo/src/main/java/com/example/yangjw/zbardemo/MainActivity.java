9628b48759e1a8a710ea7e72d4ebe464312179e6
yangjw <yangwen8701@126.com>
2016-06-07 20:19:42 +0800

    Zbar二维码扫描Demo

--- zbardemo/src/main/java/com/example/yangjw/zbardemo/MainActivity.java
+++ zbardemo/src/main/java/com/example/yangjw/zbardemo/MainActivity.java
@@ -1,13 +1,47 @@
 package com.example.yangjw.zbardemo;
 
+import android.hardware.Camera;
+import android.hardware.camera2.CameraManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity implements SurfaceHolder.Callback{
+
+    private SurfaceView surfaceView;
+    Camera camera;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        surfaceView = (SurfaceView) findViewById(R.id.surface_view);
+        surfaceView.getHolder().addCallback(this);
+    }
+
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        camera = Camera.open();
+        try {
+            camera.setPreviewDisplay(holder);
+            camera.startPreview();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        camera.stopPreview();
     }
 }
